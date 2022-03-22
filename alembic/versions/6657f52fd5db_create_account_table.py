3a2c2c2367722c6ba8309463defd7697e5bb144d
Nazar Khort <nazar.khort@gmail.com>
2022-03-22 14:09:13 +0200

    Make alembic revision

---
+++ alembic/versions/6657f52fd5db_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 6657f52fd5db
+Revises: 
+Create Date: 2022-03-22 12:21:52.586554
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6657f52fd5db'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
